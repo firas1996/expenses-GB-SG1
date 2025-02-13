b7b44e3f396c43444e1cc878a76a3e8757faf27b
firas hamila <firas.hm.1996@gmail.com>
2025-02-13 09:31:30 +0100

    first commit

---
+++ src/components/Expensetem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Expensetem = () => {
+  return (
+    <div>
+      <div>date</div>
+      <div>
+        <h3>title</h3>
+        <div>$ price</div>
+      </div>
+    </div>
+  );
+};
+
+export default Expensetem;
