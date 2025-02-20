93570a439198fdc6259be089dbb29e364d125be3
firas hamila <firas.hm.1996@gmail.com>
2025-02-20 11:02:22 +0100

    a

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, price, date }) => {
   const year = date.getFullYear();
   const month = date.toLocaleString("en-US", { month: "long" });
   const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const [newTitle, setNewTitle] = useState(title);
+
   const clickHandler = () => {
-    console.log("test");
+    console.log(newTitle);
+    setNewTitle("new title !");
+    console.log(newTitle);
   };
   return (
     <div className="expense-item">
@@ -15,7 +20,7 @@ const ExpenseItem = ({ title, price, date }) => {
         <div>{day}</div>
       </div>
       <div className="expense-item__description">
-        <h3>{title}</h3>
+        <h3>{newTitle}</h3>
         <button onClick={clickHandler}>Update Title !!</button>
         <div className="expense-item__price">$ {price}</div>
       </div>
