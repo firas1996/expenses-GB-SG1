b99f0f1620cd7635499c8cdf845e6b8c96cd3266
firas hamila <firas.hm.1996@gmail.com>
2025-02-20 11:24:46 +0100

    a

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -5,13 +5,13 @@ const ExpenseItem = ({ title, price, date }) => {
   const year = date.getFullYear();
   const month = date.toLocaleString("en-US", { month: "long" });
   const day = date.toLocaleString("en-US", { day: "2-digit" });
-  const [newTitle, setNewTitle] = useState(title);
+  // const [newTitle, setNewTitle] = useState(title);
 
-  const clickHandler = () => {
-    console.log(newTitle);
-    setNewTitle("new title !");
-    console.log(newTitle);
-  };
+  // const clickHandler = () => {
+  //   console.log(newTitle);
+  //   setNewTitle("new title !");
+  // };
+  // console.log(newTitle);
   return (
     <div className="expense-item">
       <div className="expense-date">
@@ -20,8 +20,8 @@ const ExpenseItem = ({ title, price, date }) => {
         <div>{day}</div>
       </div>
       <div className="expense-item__description">
-        <h3>{newTitle}</h3>
-        <button onClick={clickHandler}>Update Title !!</button>
+        <h3>{title}</h3>
+        {/* <button onClick={clickHandler}>Update Title !!</button> */}
         <div className="expense-item__price">$ {price}</div>
       </div>
     </div>
