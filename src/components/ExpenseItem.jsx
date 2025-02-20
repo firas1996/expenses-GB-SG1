a7c1e071fec610cfabd7d10de6766f2dde21be6e
firas hamila <firas.hm.1996@gmail.com>
2025-02-20 10:32:54 +0100

    a

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -4,6 +4,9 @@ const ExpenseItem = ({ title, price, date }) => {
   const year = date.getFullYear();
   const month = date.toLocaleString("en-US", { month: "long" });
   const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const clickHandler = () => {
+    console.log("test");
+  };
   return (
     <div className="expense-item">
       <div className="expense-date">
@@ -13,6 +16,7 @@ const ExpenseItem = ({ title, price, date }) => {
       </div>
       <div className="expense-item__description">
         <h3>{title}</h3>
+        <button onClick={clickHandler}>Update Title !!</button>
         <div className="expense-item__price">$ {price}</div>
       </div>
     </div>
