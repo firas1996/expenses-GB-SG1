d90ac2427f8f34cf11815d11c77f85aefc99a716
firas hamila <firas.hm.1996@gmail.com>
2025-02-27 09:57:20 +0100

    a

--- src/components/ExpensesContainer.jsx
+++ src/components/ExpensesContainer.jsx
@@ -1,17 +1,26 @@
 import "./ExpensesContainer.css";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const ExpensesContainer = ({ expensesData }) => {
   const years = [
     "All",
     ...new Set(expensesData.map((el) => el.date.getFullYear()).sort()),
   ];
-  console.log(years);
+  const [selectedYear, setSelectedYear] = useState(years[0]);
+  const filtredExpenses = expensesData.filter((el) => {
+    return selectedYear == "All" ? true : el.date.getFullYear() == selectedYear;
+  });
+  console.log(filtredExpenses);
   return (
     <div className="expenses">
-      <ExpensesFilter years={years} />
-      {expensesData.map((expense) => {
+      <ExpensesFilter
+        years={years}
+        selectedYear={selectedYear}
+        setSelectedYear={setSelectedYear}
+      />
+      {filtredExpenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
