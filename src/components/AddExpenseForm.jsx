696bb39e3456c7632c3d209ae004c19142e97df7
firas hamila <firas.hm.1996@gmail.com>
2025-03-06 10:48:39 +0100

    a

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,21 +1,30 @@
 import "./AddExpenseForm.css";
 
 const AddExpenseForm = () => {
+  const thisYear = new Date().getFullYear();
+  const minDate = `${thisYear - 2}-01-01`;
+  const maxDate = `${thisYear - 2}-12-31`;
   return (
     <div className="add-expense">
       <form>
         <div className="add-expense__controls">
           <div className="add-expense__control">
             <label>Title</label>
-            <input />
+            <input required placeholder="Title" />
           </div>
           <div className="add-expense__control">
             <label>Price</label>
-            <input />
+            <input
+              required
+              type="number"
+              placeholder="Price"
+              min="0"
+              step="0.01"
+            />
           </div>
           <div className="add-expense__control">
             <label>Date</label>
-            <input />
+            <input required type="date" min={minDate} max={maxDate} />
           </div>
         </div>
         <div className="add-expense__actions">
