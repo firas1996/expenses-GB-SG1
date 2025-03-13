9e21e2b8ff920d13c3e28cfabeae8c88bf9c260c
firas hamila <firas.hm.1996@gmail.com>
2025-03-13 10:32:16 +0100

    final

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -10,6 +10,10 @@ const AddExpenseForm = ({ getData }) => {
     price: "",
     date: "",
   });
+  const [showForm, setShowForm] = useState(false);
+  const showFormHandler = () => {
+    setShowForm(!showForm);
+  };
   const inputChangeHandler = ({ target }) => {
     const { name, value } = target;
     setFormData({ ...formData, [name]: value });
@@ -30,49 +34,55 @@ const AddExpenseForm = ({ getData }) => {
   };
   return (
     <div className="add-expense">
-      <form onSubmit={formSubmition}>
-        <div className="add-expense__controls">
-          <div className="add-expense__control">
-            <label>Title</label>
-            <input
-              required
-              placeholder="Title"
-              onChange={inputChangeHandler}
-              value={formData.title}
-              name="title"
-            />
+      {showForm ? (
+        <form onSubmit={formSubmition}>
+          <div className="add-expense__controls">
+            <div className="add-expense__control">
+              <label>Title</label>
+              <input
+                required
+                placeholder="Title"
+                onChange={inputChangeHandler}
+                value={formData.title}
+                name="title"
+              />
+            </div>
+            <div className="add-expense__control">
+              <label>Price</label>
+              <input
+                required
+                type="number"
+                placeholder="Price"
+                min="0"
+                step="0.01"
+                onChange={inputChangeHandler}
+                value={formData.price}
+                name="price"
+              />
+            </div>
+            <div className="add-expense__control">
+              <label>Date</label>
+              <input
+                required
+                type="date"
+                min={minDate}
+                max={maxDate}
+                onChange={inputChangeHandler}
+                value={formData.date}
+                name="date"
+              />
+            </div>
           </div>
-          <div className="add-expense__control">
-            <label>Price</label>
-            <input
-              required
-              type="number"
-              placeholder="Price"
-              min="0"
-              step="0.01"
-              onChange={inputChangeHandler}
-              value={formData.price}
-              name="price"
-            />
+          <div className="add-expense__actions">
+            <button type="button" onClick={showFormHandler}>
+              Cancel
+            </button>
+            <button type="submit">Add expense</button>
           </div>
-          <div className="add-expense__control">
-            <label>Date</label>
-            <input
-              required
-              type="date"
-              min={minDate}
-              max={maxDate}
-              onChange={inputChangeHandler}
-              value={formData.date}
-              name="date"
-            />
-          </div>
-        </div>
-        <div className="add-expense__actions">
-          <button>Cancel</button>
-          <button type="submit">Add expense</button>
-        </div>
-      </form>
+        </form>
+      ) : (
+        <button onClick={showFormHandler}>Add New Expense</button>
+      )}
     </div>
   );
 };
